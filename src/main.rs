e8adf1cc7778bfd6f3b2c462812ea51769297c14
Ubugeeei <ubuge1122@gamil.com>
2022-08-15 09:44:18 +0900

    feat: basic pokemon structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+#![allow(dead_code)]
+
+pub mod pokemon_structs {
+    pub struct Pokemon {
+        name: String,
+        level: u8,
+        status: Status,
+        skils: [Skill; 4]
+    }
+    pub struct Status {
+        h: StatusAtom,
+        a: StatusAtom,
+        b: StatusAtom,
+        c: StatusAtom,
+        d: StatusAtom,
+        s: StatusAtom,
+    }
+    pub struct StatusAtom {
+        value: u8,
+        buf: i8,
+    }
+
+    pub struct Skill {
+        name: String,
+        rbi: u8,
+        accuracy: u8,
+        type_: SkillType,
+    }
+
+    enum SkillType {
+        Physical,
+        Special,
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
