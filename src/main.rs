ae917bc05e7f36d3f55bc8873fa031167d6f279b
Ubugeeei <ubuge1122@gamil.com>
2022-08-15 10:12:50 +0900

    feat: basic pokemon skils

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,118 @@
 
 pub mod pokemon_structs {
     pub struct Pokemon {
-        name: String,
-        level: u8,
-        status: Status,
-        skils: [Skill; 4]
+        pub name: String,
+        pub level: u8,
+        pub status: Status,
+        pub skils: [Skill; 4],
     }
     pub struct Status {
-        h: StatusAtom,
-        a: StatusAtom,
-        b: StatusAtom,
-        c: StatusAtom,
-        d: StatusAtom,
-        s: StatusAtom,
+        pub h: StatusAtom,
+        pub a: StatusAtom,
+        pub b: StatusAtom,
+        pub c: StatusAtom,
+        pub d: StatusAtom,
+        pub s: StatusAtom,
     }
     pub struct StatusAtom {
-        value: u8,
-        buf: i8,
+        pub value: u8,
+        pub buf: i8,
     }
 
     pub struct Skill {
-        name: String,
-        rbi: u8,
-        accuracy: u8,
-        type_: SkillType,
+        pub name: String,
+        pub rbi: u8,
+        pub accuracy: u8,
+        pub efficacy: Option<StatusEffect>,
+        pub type_: SkillType,
+    }
+    pub enum SkillType {
+        PhysicalAttack,
+        SpecialAttack,
+        ChangeStatus,
     }
 
-    enum SkillType {
-        Physical,
-        Special,
+    pub struct StatusEffect {
+        pub efficacy: Efficacy,
+        pub target: Target,
+    }
+    pub enum Target {
+        Self_,
+        Ally,
+        Enemy,
+    }
+    pub struct Efficacy {
+        pub target: StatusEnum,
+        pub effect_value: i8,
+    }
+    pub enum StatusEnum {
+        H,
+        A,
+        B,
+        C,
+        D,
+        S,
     }
 }
 
 fn main() {
-    println!("Hello, world!");
+    use pokemon_structs::*;
+
+    let tailwind = Skill {
+        name: "しっぽをふる".to_string(),
+        rbi: 0,
+        accuracy: 100,
+        efficacy: Some(StatusEffect {
+            efficacy: Efficacy {
+                target: StatusEnum::B,
+                effect_value: -1,
+            },
+            target: Target::Ally,
+        }),
+        type_: SkillType::ChangeStatus,
+    };
+
+    let thundershock = Skill {
+        name: "でんきショック".to_string(),
+        rbi: 40,
+        accuracy: 100,
+        efficacy: None,
+        type_: SkillType::SpecialAttack,
+    };
+
+    let growl = Skill {
+        name: "なきごえ".to_string(),
+        rbi: 0,
+        accuracy: 100,
+        efficacy: Some(StatusEffect {
+            efficacy: Efficacy {
+                target: StatusEnum::A,
+                effect_value: -1,
+            },
+            target: Target::Enemy,
+        }),
+        type_: SkillType::ChangeStatus,
+    };
+
+    let quick_attack = Skill {
+        name: "でんこうせっか".to_string(),
+        rbi: 40,
+        accuracy: 100,
+        efficacy: None,
+        type_: SkillType::PhysicalAttack,
+    };
+
+    let pika = Pokemon {
+        name: "ピカチュウ".to_string(),
+        level: 10,
+        status: Status {
+            h: StatusAtom { value: 30, buf: 0 },
+            a: StatusAtom { value: 19, buf: 0 },
+            b: StatusAtom { value: 16, buf: 0 },
+            c: StatusAtom { value: 18, buf: 0 },
+            d: StatusAtom { value: 18, buf: 0 },
+            s: StatusAtom { value: 26, buf: 0 },
+        },
+        skils: [tailwind, thundershock, growl, quick_attack],
+    };
 }
